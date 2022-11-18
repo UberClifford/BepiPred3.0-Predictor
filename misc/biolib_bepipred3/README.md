1b06a85a0ac6ba388dbbad39860dd8df57cb4982
Joakim Clifford <cliffordjoakim@gmail.com>
2022-11-18 12:29:24 +0100

    updated docker image build setup

--- misc/biolib_bepipred3/README.md
+++ misc/biolib_bepipred3/README.md
@@ -4,13 +4,13 @@ BepiPred-3.0 uses the recent developments in protein language modeling to predic
 * Upload a fasta formatted file containing the protein sequence(s).
 * Set classification threshold (default is 0.1512).
 * Specify x number of top residue candidates should be included for the protein sequence(s) (default is 15). 
-
+* Use sequential smoothing (rolling mean) for B-cell epitope probability score graphs. This can make identification of epitope patches easier, but with a higher risk of false positives.
 ## Outputs
 A total of 4 files are generated, where epitope and non-epitope residues are indicated with uppercase and lowercase letters respectively.  
 * 'Bcell_epitope_preds.fasta'. Contains B-cell epitope predictions for the protein sequence(s) at the specified threshold is generated.
 * 'Bcell_epitope_top_x_preds.fasta'. Contains the top x residue candidates. 
-* 'raw_output.csv'. Contains the B-cell epitope probability scores for each residue of the protein sequence(s).
-* 'output_interactive_figures.html'. The optimal threshold is often protein specific. This html file can be opened in any browser and allows the user to manually set the threshold for each protein and get the the corresponding B-cell epitope predictions. By default, these graphs are generated for the first 30 proteins in the fasta file due to file size contraints. 
+* 'raw_output.csv'. Contains the B-cell epitope probability scores for each residue of the protein sequence(s). The rolling mean score is also provided.
+* 'output_interactive_figures.html'. The optimal threshold is often protein specific. This html file can be opened in any browser and allows the user to manually set the threshold for each protein and get the the corresponding B-cell epitope predictions. By default, these graphs are generated for the first 40 proteins in the fasta file due to file size contraints. 
 
 ## Plot information
 ![Screenshot](example_interactive_plot.png)	
@@ -18,7 +18,7 @@ In the interface for 'output_interactive_figures.html', the x and y axis are pro
 part of a B-cell epitope. The threshold can be set by using the slider bar, which moves a dashed line along the y-axis. Epitope predictions are updated accordingly, and B-cell epitope predictions at the set threshold can be downloaded by clicking the button ‘Download epitope prediction’.
 
 ## Reference
-If you found BepiPred-3.0 useful in your research, please cite this paper:
+If you found BepiPred-3.0 useful in your research, please cite our paper:
 
 
 [BepiPred-3.0: Improved B-cell epitope prediction using protein language models](https://www.biorxiv.org/content/10.1101/2022.07.11.499418v1)
--- misc/biolib_bepipred3/bepipred3_CLI.py
+++ misc/biolib_bepipred3/bepipred3_CLI.py
@@ -2,47 +2,59 @@
 from bp3 import bepipred3
 from pathlib import Path
 import argparse
-import sys
 
 WORK_DIR = Path( Path(__file__).parent.resolve() )
 
 ### COMMAND LINE ARGUMENTS ###
 parser = argparse.ArgumentParser("Make B-cell epitope predictions from fasta file.")
 parser.add_argument("-i", required=True, action="store", dest="fasta_file", type=Path, help="Fasta file contianing antigens")
-#parser.add_argument("-o", required=True, action="store", dest="out_dir", type=Path, help="Output file to store B-cell epitope predictions.")
-#parser.add_argument("-pred", action="store", choices=["mjv_pred", "vt_pred"], required=True, dest="pred", help="Majorty vote ensemble prediction or\
-#   variable threshold predicition on average ensemble posistive probabilities. ")
-#parser.add_argument("-add_seq_len", action="store_true", dest="add_seq_len", help="Add sequence lengths to esm1b-encodings. Default is false.")
-#parser.add_argument("-esm1b_dir", action="store", default= WORK_DIR / "esm1b_encodings", dest="esm1b_dir", type=Path, help="Directory to save ESM1b encodings to. Default is current working directory.")
-parser.add_argument("-t", action="store", default=0.1512, type=float, dest="var_threshold", help="Threshold to use, when making predictions on average ensemble positive probability outputs. Default is 0.1512.")
-parser.add_argument("-top", action="store", default=15, type=int, dest="top_cands", help="Number of top candidates to display in top candidate residue output file. Default is 15.")
+parser.add_argument("-o", required=True, action="store", dest="out_dir", type=Path, help="Output file to store B-cell epitope predictions.")
+parser.add_argument("-pred", action="store", choices=["mjv_pred", "vt_pred"], required=True, dest="pred", help="Majorty vote ensemble prediction or\
+	variable threshold predicition on average ensemble posistive probabilities. ")
+parser.add_argument("-add_seq_len", action="store_true", dest="add_seq_len", help="Add sequence lengths to esm-encodings. Default is false.")
+parser.add_argument("-esm_dir", action="store", default= WORK_DIR / "esm_encodings", dest="esm_dir", type=Path, help="Directory to save esm encodings to. Default is current working directory.")
+parser.add_argument("-t", action="store", default=0.1512, type=float, dest="var_threshold", help="Threshold to use, when making predictions on average ensemble positive probability outputs. Default is 0.15.")
+parser.add_argument("-top", action="store", default=15, type=int, dest="top_cands", help="Number of top candidates to display in top candidate residue output file. Default is 10.")
+parser.add_argument("-rolling_window_size", default=9, type=int, dest="rolling_window_size", help="Window size to use for rolling average on B-cell epitope probability scores. Default is 9.")
+#*for biolib*
+#parser.add_argument("-use_rolling_mean", action="store_true", dest="use_rolling_mean", help="Use rolling mean B-cell epitope probability score for plot. Default is false.")
+parser.add_argument("-use_rolling_mean", action="store", choices = ["Yes", "No"], default="No", dest="use_rolling_mean", help="Use rolling mean B-cell epitope probability score for plot. Default is false.")
+
 
 args = parser.parse_args()
 fasta_file = args.fasta_file
+out_dir = args.out_dir
 var_threshold = args.var_threshold
+pred = args.pred
+add_seq_len = args.add_seq_len
+esm_dir = args.esm_dir
 top_cands = args.top_cands
+rolling_window_size = args.rolling_window_size
 
-### Error handling ###
-
-if var_threshold >= 1 or var_threshold <= 0:
-    sys.exit(f"The threshold for B-cell epitope prediction must be a decimal between 0 and 1. Recevied value: {var_threshold}")
-
-### CONSTANTS ###
-
-add_seq_len = True
-pred = "vt_pred"
-esm1b_dir = WORK_DIR / "esm1b_encodings"
-out_dir = WORK_DIR / "output_directory"
-run_esm_model_local = WORK_DIR / "esm_model_state_dict.pt"
+#*For biolib*
+if args.use_rolling_mean == "Yes":
+	use_rolling_mean = True
+else:
+    use_rolling_mean = False
 
 ### MAIN ###
 
-## Load antigen input and create ESM-1b encodings ## 
-MyAntigens =  bepipred3.Antigens(fasta_file, esm1b_dir, add_seq_len=add_seq_len, run_esm_model_local=run_esm_model_local)
-MyBP3EnsemblePredict = bepipred3.BP3EnsemblePredict(MyAntigens)
+## Load antigen input and create ESM-2 encodings ## 
+
+#on webservices, we have the esm2 model stored locally. To work you need both esm2_t33_650M_UR50D.pt and the esm2_t33_650M_UR50D-contact-regression.pt stored in same directory
+MyAntigens = bepipred3.Antigens(fasta_file, esm_dir, add_seq_len=add_seq_len, run_esm_model_local=WORK_DIR / "models" / "esm2_t33_650M_UR50D.pt")
+
+#MyAntigens = bepipred3.Antigens(fasta_file, esm_dir, add_seq_len=add_seq_len)
+MyBP3EnsemblePredict = bepipred3.BP3EnsemblePredict(MyAntigens, rolling_window_size=rolling_window_size)
 MyBP3EnsemblePredict.run_bp3_ensemble()
 MyBP3EnsemblePredict.raw_ouput_and_top_epitope_candidates(out_dir, top_cands)
-MyBP3EnsemblePredict.bp3_pred_variable_threshold(out_dir, var_threshold=var_threshold)
+
+## B-cell epitope predictions ##
+if pred == "mjv_pred":
+    MyBP3EnsemblePredict.bp3_pred_majority_vote(out_dir)
+elif pred == "vt_pred":
+    MyBP3EnsemblePredict.bp3_pred_variable_threshold(out_dir, var_threshold=var_threshold)
 
 #generate plots (generating graphs for a maximum of 40 proteins)
-MyBP3EnsemblePredict.bp3_generate_plots(out_dir, num_interactive_figs=30)
+MyBP3EnsemblePredict.bp3_generate_plots(out_dir, num_interactive_figs=40, use_rolling_mean=use_rolling_mean)
+print("Click 'Download' to get the result files")
